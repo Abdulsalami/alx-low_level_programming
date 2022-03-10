436c067a2a6f1b47cb01c6db64e74500c5fd220a
Abdulsalam Aishat <aishatabdulsalam01@gmail.com>
2022-03-10 13:25:10 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,8 @@
 
 /**
  * main - prints the phrase
-"Programming is like building a multilingual puzzle
+"Programming is like building a 
+multilingual puzzle
 * Return: 0 if exited properly, non-zero otherwise
 */
 int main(void)
@@ -11,5 +12,7 @@ int main(void)
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
+
+
 
 
