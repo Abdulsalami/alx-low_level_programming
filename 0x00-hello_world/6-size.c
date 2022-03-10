d7376f6226299795940faaca0d041d2345a62424
Abdulsalam Aishat <aishatabdulsalam01@gmail.com>
2022-03-10 15:59:21 -0800

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,9 +2,7 @@
 
 
 /**
- *
  *main - prints the size of various types
- *
  *Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
@@ -16,4 +14,5 @@ printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
 printf("Size of a float: %d byte(s)\n", sizeof(float));
 return (0);
 }
+
 
