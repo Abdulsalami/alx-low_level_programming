bc47a940ba685d7547ee81bd430660d0a84f8b0a
Abdulsalam Aishat <aishatabdulsalam01@gmail.com>
2022-03-11 11:18:09 -0800

    positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,7 +12,7 @@ int main(void)
 int n;
 
 srand(time(0));
-n= rand() - RAND_MAX / 2;
+n = rand() - RAND_MAX / 2;
 if (n > 0)
 printf("%d is positive\n", n);
 else if (n == 0)
@@ -21,3 +21,4 @@ else if (n < 0)
 printf("%d is negative\n", n);
 return (0);
 }
+
